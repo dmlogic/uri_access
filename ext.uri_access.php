1ed654beebb9555c629f28b3a38345f3cbb31a19
Darren Miller <darren@dmlogic.com>
2013-11-13 13:33:38 +0000

    Spaces

--- ext.uri_access.php
+++ ext.uri_access.php
@@ -3,12 +3,12 @@
 /**
  * ExpressionEngine - by EllisLab
  *
- * @package		ExpressionEngine
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
- * @license		http://expressionengine.com/user_guide/license.html
- * @link		http://expressionengine.com
- * @since		Version 2.0
+ * @package     ExpressionEngine
+ * @author      ExpressionEngine Dev Team
+ * @copyright   Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @license     http://expressionengine.com/user_guide/license.html
+ * @link        http://expressionengine.com
+ * @since       Version 2.0
  * @filesource
  */
 
@@ -17,72 +17,72 @@
 /**
  * URI access Extension
  *
- * @package		ExpressionEngine
- * @subpackage	Addons
- * @category	Extension
- * @author		Darren Miller
+ * @package     ExpressionEngine
+ * @subpackage  Addons
+ * @category    Extension
+ * @author      Darren Miller
  * @link
  */
 
 class Uri_access_ext {
 
-	public $settings 		= array();
-	public $description		= 'Establishes if we\'re allowed to be at the current URI';
-	public $docs_url		= '';
-	public $name			= 'URI access';
-	public $settings_exist	= 'n';
-	public $version			= '1.0';
+    public $settings        = array();
+    public $description     = 'Establishes if we\'re allowed to be at the current URI';
+    public $docs_url        = '';
+    public $name            = 'URI access';
+    public $settings_exist  = 'n';
+    public $version         = '1.0';
 
-	/**
-	 * Constructor
-	 *
-	 * @param 	mixed	Settings array or empty string if none exist.
-	 */
-	public function __construct($settings = '')
-	{
-		$this->settings = $settings;
-	}
+    /**
+     * Constructor
+     *
+     * @param   mixed   Settings array or empty string if none exist.
+     */
+    public function __construct($settings = '')
+    {
+        $this->settings = $settings;
+    }
 
     // ----------------------------------------------------------------------
 
-	/**
-	 * Activate Extension
-	 *
-	 * This function enters the extension into the exp_extensions table
-	 *
-	 * @see http://codeigniter.com/user_guide/database/index.html for
-	 * more information on the db class.
-	 *
-	 * @return void
-	 */
-	public function activate_extension()
-	{
-		// Setup custom settings in this array.
-		$this->settings = array();
+    /**
+     * Activate Extension
+     *
+     * This function enters the extension into the exp_extensions table
+     *
+     * @see http://codeigniter.com/user_guide/database/index.html for
+     * more information on the db class.
+     *
+     * @return void
+     */
+    public function activate_extension()
+    {
+        // Setup custom settings in this array.
+        $this->settings = array();
 
-		$data = array(
-			'class'		=> __CLASS__,
-			'method'	=> 'sessions_end',
-			'hook'		=> 'sessions_end',
-			'settings'	=> serialize($this->settings),
-			'version'	=> $this->version,
-			'enabled'	=> 'y'
-		);
+        $data = array(
+            'class'     => __CLASS__,
+            'method'    => 'sessions_end',
+            'hook'      => 'sessions_end',
+            'settings'  => serialize($this->settings),
+            'version'   => $this->version,
+            'enabled'   => 'y'
+        );
 
-		ee()->db->insert('extensions', $data);
+        ee()->db->insert('extensions', $data);
 
-	}
+    }
 
-	// ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 
-	/**
-	 * sessions_end
-	 *
-	 * @param
-	 * @return
-	 */
-	public function sessions_end($session)
-	{
+    /**
+     * sessions_end
+     *
+     * @param
+     * @return
+     */
+    public function sessions_end($session)
+    {
         $compareUri = (ee()->config->item('uri_access_compare_uri')) ? ee()->config->item('uri_access_compare_uri') : ee()->uri->uri_string();
 
         $checker = new UriAccess\Checker( ee()->config->item('uri_access_map') );
@@ -93,42 +93,42 @@ class Uri_access_ext {
 
             ee()->functions->redirect(ee()->config->item('uri_access_denied_url'), FALSE, 403);
         }
-	}
+    }
 
-	// ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 
-	/**
-	 * Disable Extension
-	 *
-	 * This method removes information from the exp_extensions table
-	 *
-	 * @return void
-	 */
-	function disable_extension()
-	{
-		ee()->db->where('class', __CLASS__);
-		ee()->db->delete('extensions');
-	}
+    /**
+     * Disable Extension
+     *
+     * This method removes information from the exp_extensions table
+     *
+     * @return void
+     */
+    function disable_extension()
+    {
+        ee()->db->where('class', __CLASS__);
+        ee()->db->delete('extensions');
+    }
 
-	// ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 
-	/**
-	 * Update Extension
-	 *
-	 * This function performs any necessary db updates when the extension
-	 * page is visited
-	 *
-	 * @return 	mixed	void on update / false if none
-	 */
-	function update_extension($current = '')
-	{
-		if ($current == '' OR $current == $this->version)
-		{
-			return FALSE;
-		}
-	}
+    /**
+     * Update Extension
+     *
+     * This function performs any necessary db updates when the extension
+     * page is visited
+     *
+     * @return  mixed   void on update / false if none
+     */
+    function update_extension($current = '')
+    {
+        if ($current == '' OR $current == $this->version)
+        {
+            return FALSE;
+        }
+    }
 
-	// ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 }
 
 /* End of file ext.Uri_access.php */
